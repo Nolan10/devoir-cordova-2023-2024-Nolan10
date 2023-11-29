72055dc0607a007ef61b0911e63189df9471f881
Nolan10 <nolan.tessier.etu@univ-lemans.fr>
2023-11-29 17:57:05 -0500

    dernier commit

---
+++ mon-nouveau-projet-save/www/vue/VueListeJoueur.js
@@ -0,0 +1,28 @@
+class VueListeJoueur {
+    constructor() {
+        this.html = document.getElementById("html-vue-liste-joueur").innerHTML;
+        this.listeJoueurDonnee = null;
+    }
+
+    initialiserListeJoueur(listeJoueurDonnee){
+        this.listeJoueurDonnee = listeJoueurDonnee;
+    }
+
+    afficher(){
+        document.getElementsByTagName("body")[0].innerHTML = this.html;
+
+        let listeJoueur = document.getElementById("liste-joueur");
+        const listeJoueurItemHTML = listeJoueur.innerHTML;
+        let listeJoueurHTMLRemplacement = "";
+
+        for(var numeroJoueur in this.listeJoueurDonnee){
+            let listeJoueurItemHTMLRemplacement = listeJoueurItemHTML;
+            listeJoueurItemHTMLRemplacement = listeJoueurItemHTMLRemplacement.replace("{Joueur.id}",this.listeJoueurDonnee[numeroJoueur].id);
+            listeJoueurItemHTMLRemplacement = listeJoueurItemHTMLRemplacement.replace("{Joueur.nom}",this.listeJoueurDonnee[numeroJoueur].nom);
+            listeJoueurItemHTMLRemplacement = listeJoueurItemHTMLRemplacement.replace("{Joueur.prenom}",this.listeJoueurDonnee[numeroJoueur].prenom);
+            listeJoueurItemHTMLRemplacement = listeJoueurItemHTMLRemplacement.replace("{Joueur.id}",this.listeJoueurDonnee[numeroJoueur].id);
+            listeJoueurHTMLRemplacement += listeJoueurItemHTMLRemplacement;
+        }
+        listeJoueur.innerHTML = listeJoueurHTMLRemplacement;
+    }
+}
