56ed1f0f92c7d698da1ead0eecf95dc9836e0d88
Nolan10 <nolan.tessier.etu@univ-lemans.fr>
2024-03-08 15:44:35 -0500

    Suppression des dossiers inutiles

--- code de départ joueur/modele/Joueur.js
+++
@@ -1,9 +0,0 @@
-class Joueur {
-    constructor(nom, prenom, club, nation, id) {
-        this.id = id;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.club = club;
-        this.nation = nation;
-    }
-}
